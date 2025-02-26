e22294ddc664c4ad9d4b618dd0b25537823fd2ea
Kerim Čamdžić <hl2.for.you@gmail.com>
2025-02-26 21:36:59 +0100

    baseattachment networking

--- sp/src/game/shared/modular_weapons/baseattachment.cpp
+++ sp/src/game/shared/modular_weapons/baseattachment.cpp
@@ -4,3 +4,35 @@
 
 #include "tier0/memdbgon.h"
 
+IMPLEMENT_NETWORKCLASS_ALIASED(BaseWeaponAttachment, DT_BaseWeaponAttachment)
+
+BEGIN_NETWORK_TABLE(CBaseWeaponAttachment, DT_BaseWeaponAttachment)
+#ifdef GAME_DLL
+#endif
+#ifdef CLIENT_DLL
+#endif // CLIENT_DLL
+END_NETWORK_TABLE()
+
+#ifdef CLIENT_DLL
+BEGIN_PREDICTION_DATA(CBaseWeaponAttachment)
+//DEFINE_PRED_FIELD(m_flTimeWeaponIdle, FIELD_FLOAT, FTYPEDESC_OVERRIDE | FTYPEDESC_NOERRORCHECK),
+END_PREDICTION_DATA()
+#endif
+
+//LINK_ENTITY_TO_CLASS(weapon_base, CBaseModularWeapon);
+
+#ifdef GAME_DLL
+BEGIN_DATADESC(CBaseWeaponAttachment)
+END_DATADESC()
+#endif
+
+
+float CBaseWeaponAttachment::GetDamageModifier()
+{
+	return 0.0f;
+}
+
+AttachmentType_t CBaseWeaponAttachment::GetAttachmentType()
+{
+	return ATTACHMENT_NONE;
+}
